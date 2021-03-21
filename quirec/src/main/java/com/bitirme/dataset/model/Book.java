93646bd0827270022e564d018a91c9aa7483b641
pserimer <p_serimer@outlook.com>
2021-03-22 00:04:22 +0300

    Books model oluşturma

--- quirec/src/main/java/com/bitirme/dataset/model/Book.java
+++ quirec/src/main/java/com/bitirme/dataset/model/Book.java
@@ -1,10 +1,12 @@
 package com.bitirme.dataset.model;
 
 import com.bitirme.base.model.BaseModel;
+import com.bitirme.quirec.questionnarie.model.Categories;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -14,4 +16,14 @@ import javax.persistence.Table;
 @EqualsAndHashCode(callSuper = false, of = {})
 @ToString(callSuper = true, of = {})
 public class Book extends BaseModel {
+
+    @Column(name = "RESOURCE_ID") //TODO: Search&details için kullanılacak
+    public String resourceId;
+
+    @Column(name = "TITLE")
+    public String title;
+
+    @Column(name = "AUTHOR")
+    public String author;
+
 }
