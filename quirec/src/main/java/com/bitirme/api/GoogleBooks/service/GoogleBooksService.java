94c2868cfa3c28d975b49c2850ef5112b90b9f2d
pserimer <p_serimer@outlook.com>
2021-03-23 19:03:47 +0300

    GoogleBooks service arrangements

---
+++ quirec/src/main/java/com/bitirme/api/GoogleBooks/service/GoogleBooksService.java
@@ -0,0 +1,9 @@
+package com.bitirme.api.GoogleBooks.service;
+
+public interface GoogleBooksService {
+
+    void getBooks();
+
+    void getBookDetail(String resourceId);
+
+}
--- quirec/src/main/java/com/bitirme/quirec/api/GoogleBooks/service/GoogleBooksService.java
+++
@@ -1,7 +0,0 @@
-package com.bitirme.quirec.api.GoogleBooks.service;
-
-public interface GoogleBooksService {
-
-    void getBooks();
-
-}
