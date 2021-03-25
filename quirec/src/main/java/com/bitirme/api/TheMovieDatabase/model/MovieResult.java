c1a244b08b4d6a0a25d79d34f25bd90f573096e5
pserimer <p_serimer@outlook.com>
2021-03-25 12:06:27 +0300

    Started implementing movie part

---
+++ quirec/src/main/java/com/bitirme/api/TheMovieDatabase/model/MovieResult.java
@@ -0,0 +1,33 @@
+package com.bitirme.api.TheMovieDatabase.model;
+
+import java.util.List;
+
+public class MovieResult {
+
+    private int id;
+
+    private String title;
+
+    private String original_title;
+
+    private String poster_path;
+
+    private boolean adult;
+
+    private String overview;
+
+    private String release_date;
+
+    private List<Integer> genre_ids;
+
+    private String original_language;
+
+    private String backdrop_path;
+
+    private int vote_count;
+
+    private boolean video;
+
+    private int vote_average;
+
+}
--- quirec/src/main/java/com/bitirme/dataset/model/Movie.java
+++ quirec/src/main/java/com/bitirme/dataset/model/Movie.java
@@ -1,12 +1,14 @@
 package com.bitirme.dataset.model;
 
 import com.bitirme.base.model.BaseModel;
+import com.bitirme.quirec.questionnarie.model.Categories;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(schema = "QUIREC", name = "MOVIE")
@@ -14,4 +16,25 @@ import javax.persistence.Table;
 @EqualsAndHashCode(callSuper = false, of = {})
 @ToString(callSuper = true, of = {})
 public class Movie extends BaseModel {
+
+    @Column(name = "RESOURCE_ID")
+    public String resourceId;
+
+    @Column(name = "TITLE")
+    public String title;
+
+    @Column(name = "POSTER")
+    public String poster;
+
+    @Column(name = "VIDEO")
+    public boolean video;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            schema = "QUIREC", name = "MOVIE_CATEGORIES",
+            joinColumns = {@JoinColumn(name = "MOVIE_ID", foreignKey = @ForeignKey(name = "FK_MOVIE_CATEGORIES_MOVIE_ID"))},
+            inverseJoinColumns = {@JoinColumn(name = "CATEGORY_ID", foreignKey = @ForeignKey(name = "FK_MOVIE_CATEGORIES_CATEGORY_ID"))}
+    )
+    private Set<Categories> categories = new HashSet<>();
+
 }
