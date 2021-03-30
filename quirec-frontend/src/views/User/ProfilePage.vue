e866e3a5dcfecf1353b0fc7e1c6738a348d696af
yarenipek <41819415+yarenipek@users.noreply.github.com>
2021-03-30 15:22:33 +0300

    Delete Account Page is added

--- quirec-frontend/src/views/User/ProfilePage.vue
+++ quirec-frontend/src/views/User/ProfilePage.vue
@@ -6,26 +6,45 @@
         <v-simple-table class="mx-5 my-10 align-center justify-center">
           <template v-slot:default>
             <tbody>
-              <tr>
-                <td class="text-left">Username</td>
-                <td class="text-left">{{username}}</td>
-              </tr>
-              <tr>
-                <td>E-mail</td>
-                <td>{{email}}</td>
-              </tr>
+            <tr>
+              <td class="text-left">Username</td>
+              <td class="text-left">{{ username }}</td>
+            </tr>
+            <tr>
+              <td>E-mail</td>
+              <td>{{ email }}</td>
+            </tr>
             </tbody>
           </template>
         </v-simple-table>
-        <v-card-actions class="align-end justify-end mx-10">
-          <v-btn to="/Questionnaire" large color="#c2185b" rounded>Change Questionnaire</v-btn>
-        </v-card-actions>
-        <v-card-actions class="align-end justify-end mx-10">
-          <v-btn to="/ChangePass" large color="#c2185b" rounded>Change Password</v-btn>
-        </v-card-actions>
-        <v-card-actions class="align-left justify-left mx-5">
-          <v-btn large color="#c2185b" rounded>Go Back To Main Page</v-btn>
-        </v-card-actions>
+        <v-simple-table>
+          <tr>
+            <td>
+              <v-card-actions class="align-left justify-left mx-5">
+                <v-btn large color="#c2185b" rounded>Go Back To Main Page</v-btn>
+              </v-card-actions>
+            </td>
+
+            <td>
+              <v-card-actions class="align-end justify-end mx-10">
+                <v-btn to="/Questionnaire" large color="#c2185b" rounded>Change Questionnaire</v-btn>
+              </v-card-actions>
+            </td>
+          </tr>
+
+          <tr>
+            <td>
+              <v-card-actions class="align-left justify-left mx-5">
+                <v-btn to="/DeleteAccount" large color="#c2185b" rounded>Delete My Account</v-btn>
+              </v-card-actions>
+            </td>
+            <td>
+              <v-card-actions class="align-end justify-end mx-10">
+                <v-btn to="/ChangePass" large color="#c2185b" rounded>Change Password</v-btn>
+              </v-card-actions>
+            </td>
+          </tr>
+        </v-simple-table>
       </v-card>
     </v-flex>
   </v-container>
@@ -36,18 +55,18 @@ export default {
   name: "profilepage",
   data() {
     return {
-        username:"",
-        email: ""
+      username: "",
+      email: ""
     };
   },
-  async mounted () {
+  async mounted() {
     this.getUserInfo();
   },
   methods: {
-    async getUserInfo(){
-        const response = await this.axios.get('http://localhost:9000/quirec-api/user/' + this.$store.getters.getUserId)
-        this.username = response.data.username;
-        this.email = response.data.email;
+    async getUserInfo() {
+      const response = await this.axios.get('http://localhost:9000/quirec-api/user/' + this.$store.getters.getUserId)
+      this.username = response.data.username;
+      this.email = response.data.email;
 
     }
   }
