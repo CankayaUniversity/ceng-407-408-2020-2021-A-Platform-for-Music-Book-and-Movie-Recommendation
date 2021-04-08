2c71fb0a6868b996d6ace40076f45096158eacc8
Pelinsu Serimer <48836119+pserimer@users.noreply.github.com>
2021-04-09 02:11:00 +0300

    movie api, completed

--- quirec/src/main/java/com/bitirme/api/TheMovieDatabase/impl/TheMovieDatabaseServiceImpl.java
+++ quirec/src/main/java/com/bitirme/api/TheMovieDatabase/impl/TheMovieDatabaseServiceImpl.java
@@ -1,6 +1,8 @@
 package com.bitirme.api.TheMovieDatabase.impl;
 
 import com.bitirme.api.JSONParser.JSONParsingService;
+import com.bitirme.api.TheMovieDatabase.dao.OriginalCategoriesDao;
+import com.bitirme.api.TheMovieDatabase.model.OriginalCategories;
 import com.bitirme.api.TheMovieDatabase.service.TheMovieDatabaseService;
 import com.bitirme.dataset.dao.MovieDao;
 import com.bitirme.dataset.model.Movie;
@@ -10,6 +12,7 @@ import com.bitirme.quirec.questionnarie.model.CategoryType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
@@ -27,10 +30,15 @@ public class TheMovieDatabaseServiceImpl implements TheMovieDatabaseService {
     @Autowired
     CategoryDao categoryDao;
 
+    @Autowired
+    OriginalCategoriesDao originalCategoriesDao;
+
     @Override
     public void getMovies() {
-        String getMoviesUrl = "https://api.themoviedb.org/3/movie/popular?api_key=7f613a475981323801d5497ba77b29b5&page=1";
-        LinkedHashMap<String, ArrayList<LinkedHashMap<String, Object>>> resource = (LinkedHashMap<String, ArrayList<LinkedHashMap<String, Object>>>) parsingService.get(getMoviesUrl);
+        int page = (int) ((movieDao.count() / 20) + 1);
+
+        String getMoviesUrl = "https://api.themoviedb.org/3/movie/popular?api_key=7f613a475981323801d5497ba77b29b5&page={page}";
+        LinkedHashMap<String, ArrayList<LinkedHashMap<String, Object>>> resource = (LinkedHashMap<String, ArrayList<LinkedHashMap<String, Object>>>) parsingService.getForPage(getMoviesUrl, page);
 
         ArrayList<LinkedHashMap<String, Object>> movies = resource.get("results");
 
@@ -39,7 +47,7 @@ public class TheMovieDatabaseServiceImpl implements TheMovieDatabaseService {
             Movie existingMovie = movieDao.findMoviesByResourceId(resourceId);
 
             if(existingMovie == null){
-                LinkedHashMap<String, ArrayList> categories = (LinkedHashMap<String, ArrayList>) movie.get("genre_ids");
+                ArrayList<Integer> categories = (ArrayList<Integer>) movie.get("genre_ids");
 
                 if(categories != null){
                     Movie newMovie = new Movie();
@@ -49,16 +57,52 @@ public class TheMovieDatabaseServiceImpl implements TheMovieDatabaseService {
                     newMovie.setPoster((String) movie.get("poster_path"));
                     newMovie.setLanguage((String) movie.get("original_language"));
 
+                    categories
+                            .forEach(
+                                    categoryId -> {
+                                        OriginalCategories originalC = originalCategoriesDao.findByOriginalId(categoryId);
 
+                                        if (originalC != null) {
+                                            Categories c = categoryDao.findCategoriesByCategoryTypeAndOriginalId(CategoryType.MOVIE, categoryId);
+                                            newMovie.getCategories().add(c);
+                                            movieDao.saveAndFlush(newMovie);
+                                        }
+
+                                        else
+                                            throw new EntityNotFoundException("original category not found");
+                                    }
+                            );
                 }
-
-
-
             }
         }
     }
 
+    private void getOriginalCategories() {
+        String getCategoriesUrl = "https://api.themoviedb.org/3/genre/movie/list?api_key=7f613a475981323801d5497ba77b29b5";
+        LinkedHashMap<String, ArrayList<LinkedHashMap<String, Object>>> resource = (LinkedHashMap<String, ArrayList<LinkedHashMap<String, Object>>> ) parsingService.get(getCategoriesUrl);
 
+        ArrayList<LinkedHashMap<String, Object>> categories = resource.get("genres");
 
+        categories
+                .forEach(
+                    category -> {
+                        int id = Integer.parseInt(String.valueOf(category.get("id")));
+                        OriginalCategories originalC = originalCategoriesDao.findByOriginalId(id);
 
+                        if(originalC == null) {
+                            OriginalCategories newC = new OriginalCategories();
+                            newC.setOriginalId(id);
+                            newC.setName(category.get("name").toString());
+
+                            Categories c = new Categories();
+                            c.setCategoryType(CategoryType.MOVIE);
+                            c.setName(newC.getName());
+                            c.setOriginalId(id);
+
+                            originalCategoriesDao.saveAndFlush(newC);
+                            categoryDao.saveAndFlush(c);
+                        }
+                    }
+        );
+    }
 }
