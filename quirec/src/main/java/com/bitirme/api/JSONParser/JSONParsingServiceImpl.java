2b80088742a46706550fe1e3fb9512661161af2c
pserimer <p_serimer@outlook.com>
2021-05-15 18:23:19 +0300

    csv file to recommender server

--- quirec/src/main/java/com/bitirme/api/JSONParser/JSONParsingServiceImpl.java
+++ quirec/src/main/java/com/bitirme/api/JSONParser/JSONParsingServiceImpl.java
@@ -13,6 +13,12 @@ public class JSONParsingServiceImpl implements JSONParsingService {
     }
 
     @Override
+    public String postForRecommendation(String url,  String datasetPath) {
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.postForObject(url, datasetPath, String.class);
+    }
+
+    @Override
     public Object getForPage(String url, int page) {
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForObject(url, Object.class, page);
