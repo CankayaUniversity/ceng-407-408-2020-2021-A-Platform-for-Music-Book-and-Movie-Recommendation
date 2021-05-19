a83e5fe7e5f85abbf93a4872ba35bce27ad99603
pserimer <p_serimer@outlook.com>
2021-05-19 22:00:22 +0300

    recommendation controller düzeltme

--- quirec/src/main/java/com/bitirme/quirec/recommendation/controller/RecommendationController.java
+++ quirec/src/main/java/com/bitirme/quirec/recommendation/controller/RecommendationController.java
@@ -12,8 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.io.IOException;
-
 @Controller
 @RequestMapping("/quirec-api/recommendation")
 public class RecommendationController {
@@ -22,16 +20,17 @@ public class RecommendationController {
     RecommendationService recommendationService;
 
     @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
-    public ResponseEntity<Recommendation> get(@PathVariable("id") long userId) throws IOException {
+    public ResponseEntity<Recommendation> get(@PathVariable("id") long userId) {
         return new ResponseEntity<>(
                 recommendationService.get(userId),
                 HttpStatus.OK
         );
     }
 
-    @RequestMapping(value = "/rate", method = RequestMethod.POST)
-    public ResponseEntity<Void> rate(@RequestBody CategoryType type, int rate) {
-        recommendationService.rate(type, rate);
+    // TODO: user rating dosyasının güncellenmesi
+    @RequestMapping(value = "/user/{id}/rate", method = RequestMethod.POST)
+    public ResponseEntity<Void> rate(@PathVariable("id") long userId, @RequestBody CategoryType type, double rate) {
+        recommendationService.rate(userId, type, rate);
         return new ResponseEntity<>(
                 HttpStatus.OK
         );
--- quirec/src/main/java/com/bitirme/quirec/recommendation/service/RecommendationService.java
+++ quirec/src/main/java/com/bitirme/quirec/recommendation/service/RecommendationService.java
@@ -3,12 +3,10 @@ package com.bitirme.quirec.recommendation.service;
 import com.bitirme.quirec.questionnarie.model.CategoryType;
 import com.bitirme.quirec.recommendation.model.Recommendation;
 
-import java.io.IOException;
-
 public interface RecommendationService {
 
-    Recommendation get(long userId) throws IOException;
+    Recommendation get(long userId);
 
-    void rate(CategoryType type, double rate);
+    void rate(long userId, CategoryType type, double rate);
 
 }
