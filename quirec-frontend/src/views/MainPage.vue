6d208a031c08daa0bf6d75c9c545087a05c64d94
yarenipek <41819415+yarenipek@users.noreply.github.com>
2021-06-10 21:41:52 +0300

    Main Page view

--- quirec-frontend/src/views/MainPage.vue
+++ quirec-frontend/src/views/MainPage.vue
@@ -2,29 +2,20 @@
   <v-container>
     <v-flex>
       <v-layout class="d-flex justify-end pa-2">
-        <v-btn large color="#c2185b" rounded>Login</v-btn>
-        <v-btn class="ml-10" large color="#c2185b" rounded>Sign Up</v-btn>
+        <v-btn to="/login" large color="#c2185b" rounded>Login</v-btn>
+        <v-btn to="/register" class="ml-10" large color="#c2185b" rounded>Sign Up</v-btn>
       </v-layout>
+      <v-row align="center" justify="space-around">
+        <v-card class="my-15 mx-15 elevation-0 " max-width="700">
+          <v-img align-center :src="require('@/assets/QuiRec.png')"></v-img>
+        </v-card>
 
-      <v-card class="my-15 mx-15 elevation-0">
-        <v-img :src="require('@/assets/QuiRec.png')"></v-img>
-      </v-card>
+      </v-row>
 
       <p class="text-center text-h4 font-weight-bold">
         A Quick, handy recommender.<br/>
         One application with countless Movies, Music, Books.
       </p>
-      <v-row class="mt-15">
-        <v-col cols="4">
-          <v-img :src="require('@/assets/clapperboard.png')"></v-img>
-        </v-col>
-        <v-col cols="4">
-          <v-img :src="require('@/assets/rounded-headphones.png')"></v-img>
-        </v-col>
-        <v-col cols="4">
-          <v-img :src="require('@/assets/book.png')"></v-img>
-        </v-col>
-      </v-row>
     </v-flex>
   </v-container>
 </template>
